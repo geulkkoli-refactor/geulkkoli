d73d3b8cbf317121dbc7b58d3acc81d40d14761f
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2023-07-19 18:51:56 +0900

    fix: 캘린더 dto 값 변환을 생성자에서 바로 하게 바꾸었다.

--- src/main/java/com/geulkkoli/application/security/config/CustomAuthenticationFilter.java
+++ src/main/java/com/geulkkoli/application/security/config/CustomAuthenticationFilter.java
@@ -1,15 +1,9 @@
 package com.geulkkoli.application.security.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
-import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
-import org.springframework.stereotype.Component;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
