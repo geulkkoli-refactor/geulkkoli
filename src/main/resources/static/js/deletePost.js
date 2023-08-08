55f59941665f9f2f4a9638cc7c8dd163ebd8ea0e
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2023-08-08 22:12:51 +0900

    #11-doc: 블로그 게시글에 삭제 버튼 및 삭제 관련 자바스크립트 추가. 및 그에 따란 히든 값 게시글 아이디, 별명 추가.

---
+++ src/main/resources/static/js/deletePost.js
@@ -0,0 +1,37 @@
+document.getElementById("deletePostButton").addEventListener("click", deletePost);
+function deletePost() {
+    const deleteTitle = document.getElementById("content-title").outerText;
+    const postId = document.getElementById("post-id").getAttribute("name");
+    console.log("postId : " + postId);
+    const userNickName = document.getElementById("nickName").getAttribute("name");
+    const compareTitle = document.getElementById("post-Compare").value;
+    console.log(compareTitle);
+    console.log(deleteTitle)
+    var headerName = document.getElementsByClassName("csrf_input")[1].getAttribute("name");
+    var token = document.getElementsByClassName("csrf_input")[0].getAttribute("value");
+    let params = {
+        postId: postId,
+        userNickName: userNickName
+    };
+    let query = Object.keys(params)
+        .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(params[key]))
+        .join('&');
+    if (compareTitle === deleteTitle) {
+        fetch('/post/request?' + query, {
+            headers: {
+                'header': headerName,
+                'X-Requested-With': 'XMLHttpRequest',
+                'X-CSRF-Token': token
+            },
+            method: 'DELETE'
+        })
+            .then(response => {
+                window.location = response.url;
+            })
+            .then(
+                alert("삭제되었습니다.")
+            )
+    } else {
+        alert("제목이 일치하지 않습니다.");
+    }
+}
